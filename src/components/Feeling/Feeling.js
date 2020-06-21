7b359cb42535ec01543744db792851c6534c4bb4
Bailey Niemiller <baileyniemiller@gmail.com>
2020-06-21 17:05:27 -0500

    input validation working for feeling

--- src/components/Feeling/Feeling.js
+++ src/components/Feeling/Feeling.js
@@ -2,7 +2,14 @@ import React from 'react';
 import {connect} from 'react-redux';
 import { HashRouter as Route, Link } from "react-router-dom";
 import Header from "../Header/Header.js";
-import { withRouter } from "react-router-dom";
+import { withRouter } from "react-router";
+// import { makeStyles } from "@material-ui/core/styles";
+import InputLabel from "@material-ui/core/InputLabel";
+import MenuItem from "@material-ui/core/MenuItem";
+import FormHelperText from "@material-ui/core/FormHelperText";
+import FormControl from "@material-ui/core/FormControl";
+import Select from "@material-ui/core/Select";
+import Button from "@material-ui/core/Button";
 
 // Feelings component 
 
@@ -21,12 +28,19 @@ class Feeling extends React.Component {
     });
   }; // end handleChange
 
-
   // in the render function, there is the input where
   // the user can type 1-6.
   // On click of the "next" button then runs a dispatch to the feeling
   // reducer sending the new state (whatever the user typed)
   // as the payload.
+  submitInfo = (event) => {
+    //prevents default action
+    event.preventDefault();
+    this.props.history.push("/understanding"); //path to next page
+  };
+   componentWillUnmount() {
+    this.props.dispatch({ type: "ADD_FEELING", payload: this.state.feeling }); //sends to Redux state
+  }
 
   render() {
     return (
@@ -35,24 +49,39 @@ class Feeling extends React.Component {
           <Header />
           <div className="feelingBody">
             <h2>On a scale of 1-6, how are you feeling today?</h2>
-            <input
-              type="number"
-              placeholder="Feeling?"
-              className="feelingIn"
-              onChange={(event) => this.handleChange("feeling", event)}
-            />
-            <Link to="/understanding">
-              <button
-                onClick={() => {
-                  this.props.dispatch({
-                    type: "ADD_FEELING",
-                    payload: this.state.feeling,
-                  });
-                }}
+            <form onSubmit={this.submitInfo}>
+              {/* // onSubmit={(event) => {
+            //   event.preventDefault()
+            //   this.props.dispatch({
+            //     type: "ADD_FEELING",
+            //     payload: this.state.feeling,
+            //   });
+            //     this.props.history.push("/understanding");
+            // }} */}
+
+              <InputLabel id="feelingLabel">Feeling?</InputLabel>
+              <Select
+                required
+                labelId="feelingSelectLabel"
+                id="demo-simple-select-required"
+                value={this.state.feeling}
+                onChange={(event) => this.handleChange("feeling", event)}
               >
+                {/* <MenuItem value="">
+                  <em>None</em>
+                </MenuItem> */}
+                <MenuItem value={6}>6</MenuItem>
+                <MenuItem value={5}>5</MenuItem>
+                <MenuItem value={4}>4</MenuItem>
+                <MenuItem value={3}>3</MenuItem>
+                <MenuItem value={2}>2</MenuItem>
+                <MenuItem value={1}>1</MenuItem>
+              </Select>
+              <FormHelperText>Required</FormHelperText>
+              <Button variant="contained" color="primary"  type="submit">
                 Next
-              </button>
-            </Link>
+              </Button>
+            </form>
           </div>
         </div>
       </Route>
@@ -66,4 +95,6 @@ const mapStateToProps = (state) => {
   };
 };
 
-export default connect(mapStateToProps)(withRouter(Feeling));
+export default withRouter(connect(mapStateToProps)(Feeling));
+
+// export default connect(mapStateToProps)(withRouter(Feeling));
